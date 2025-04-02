0d5b08d5c9308f1eda520b922a169256461e73e4
Ogeid03 <45824052+Ogeid03@users.noreply.github.com>
2025-04-02 17:24:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SIW-FUP
+ Un sistema in grado di aiutare i proprietari di animali nella sconvolgente situazione dello smarrimento dei loro amici animali. Nasce con la possibilità di sensibilizzare e sviluppare la collaborazione.
---
+++ siw_group_project/src/main/java/it/uniroma3/siw_group_project/SiwGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw_group_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwGroupProjectApplication.class, args);
+	}
+
+}
