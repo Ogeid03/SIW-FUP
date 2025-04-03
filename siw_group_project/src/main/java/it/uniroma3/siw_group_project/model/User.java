97057c4ad0f3e2560c3f1a82d3d826b14592a4b3
Ogeid03 <45824052+Ogeid03@users.noreply.github.com>
2025-04-03 18:52:49 +0200

    Start

--- siw_group_project/src/main/java/it/uniroma3/siw_group_project/model/User.java
+++ siw_group_project/src/main/java/it/uniroma3/siw_group_project/model/User.java
@@ -1,5 +1,72 @@
 package it.uniroma3.siw_group_project.model;
 
-public class User {
+import java.util.Objects;
 
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeUtente;
+    //private String password;
+    private String email;
+    private String telefono;
+    private Role ruolo;
+
+    // Getter e Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeUtente() {
+        return nomeUtente;
+    }
+
+    public void setNomeUtente(String nomeUtente) {
+        this.nomeUtente = nomeUtente;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Role getRuolo() {
+        return ruolo;
+    }
+
+    public void setRuolo(Role ruolo) {
+        this.ruolo = ruolo;
+    }
+
+    // Equals basato su nomeUtente
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User utente = (User) o;
+        return Objects.equals(nomeUtente, utente.nomeUtente);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nomeUtente);
+    }
 }
