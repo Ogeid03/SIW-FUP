e485562f12bad3df010d9bdc59c4b4891466bc32
TeoCerre <matteo.cerretani@gmail.com>
2025-04-04 11:17:19 +0200

    Denuncia_Messaggio_Stato

--- siw_group_project/src/main/java/it/uniroma3/siw/model/Denuncia.java
+++ siw_group_project/src/main/java/it/uniroma3/siw/model/Denuncia.java
@@ -1,5 +1,35 @@
 package it.uniroma3.siw.model;
 
-public class Denuncia {
+import jakarta.persistence.Entity;
 
+@Entity
+public class Denuncia extends Segnalazione {
+    private String nome;
+    private Double premioOfferto;
+    private Integer eta;
+
+    // Getter e Setter
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPremioOfferto() {
+        return premioOfferto;
+    }
+
+    public void setPremioOfferto(Double premioOfferto) {
+        this.premioOfferto = premioOfferto;
+    }
+
+    public Integer getEta() {
+        return eta;
+    }
+
+    public void setEta(Integer eta) {
+        this.eta = eta;
+    }
 }
--- siw_group_project/src/main/java/it/uniroma3/siw/model/Messaggio.java
+++ siw_group_project/src/main/java/it/uniroma3/siw/model/Messaggio.java
@@ -1,5 +1,78 @@
 package it.uniroma3.siw.model;
 
-public class Message {
+import java.time.LocalDateTime;
+import java.util.Objects;
+import jakarta.persistence.*;
 
+@Entity
+public class Messaggio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String contenuto;
+    private LocalDateTime dataOra;
+    
+    @ManyToOne
+    @JoinColumn(name = "codUtente")
+    private Utente codUtente;
+    
+    @ManyToOne
+    @JoinColumn(name = "codSegnalazione")
+    private Segnalazione codSegnalazione;
+
+    // Getter e Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContenuto() {
+        return contenuto;
+    }
+
+    public void setContenuto(String contenuto) {
+        this.contenuto = contenuto;
+    }
+
+    public LocalDateTime getDataOra() {
+        return dataOra;
+    }
+
+    public void setDataOra(LocalDateTime dataOra) {
+        this.dataOra = dataOra;
+    }
+
+    public Utente getCodUtente() {
+        return codUtente;
+    }
+
+    public void setCodUtente(Utente codUtente) {
+        this.codUtente = codUtente;
+    }
+
+    public Segnalazione getCodSegnalazione() {
+        return codSegnalazione;
+    }
+
+    public void setCodSegnalazione(Segnalazione codSegnalazione) {
+        this.codSegnalazione = codSegnalazione;
+    }
+
+    // Equals e hashCode basati su ID
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Messaggio that = (Messaggio) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
