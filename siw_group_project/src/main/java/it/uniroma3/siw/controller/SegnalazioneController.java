997974176ea78cd8473b8f5b7818c6f4fb5294aa
TeoCerre <matteo.cerretani@gmail.com>
2025-04-30 12:13:38 +0200

    GestioneSegnalazione

--- siw_group_project/src/main/java/it/uniroma3/siw/controller/SegnalazioneController.java
+++ siw_group_project/src/main/java/it/uniroma3/siw/controller/SegnalazioneController.java
@@ -1,12 +1,31 @@
 package it.uniroma3.siw.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import org.springframework.ui.Model;
+import it.uniroma3.siw.model.Segnalazione;
+import it.uniroma3.siw.repository.SegnalazioneRepository;
 
 @Controller
 public class SegnalazioneController {
 
+    @Autowired
+    private SegnalazioneRepository segnalazioneRepository;
+
     @GetMapping("/segnalazioni")
-    public String mostraFormSegnalazione() {
-        return "segnalazioneForm"; // Nome del file HTML senza estensione
+    public String mostraFormSegnalazione(Model model) {
+        model.addAttribute("segnalazione", new Segnalazione());
+        return "segnalazioneForm";
+    }
+
+    @PostMapping("/segnalazioni")
+    public String salvaSegnalazione(@ModelAttribute Segnalazione segnalazione) {
+        segnalazioneRepository.save(segnalazione);
+        return "redirect:/conferma"; // oppure torna a una pagina di conferma
     }
 }
+
--- siw_group_project/src/main/java/it/uniroma3/siw/repository/AvvistamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package it.uniroma3.siw.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.uniroma3.siw.model.Avvistamento;
-
-public interface AvvistamentoRepository extends JpaRepository<Avvistamento, Long> {
-}
