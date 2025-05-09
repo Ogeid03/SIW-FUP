2bfe0a6d70326622191db38a026375e73e9c045e
Ogeid03 <45824052+Ogeid03@users.noreply.github.com>
2025-05-09 12:00:35 +0200

    alcuni update all'interfaccia

--- siw_group_project/src/main/resources/data.sql
+++ siw_group_project/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO utente (nome_utente, email, telefono, password, ruolo)
-VALUES ('mario.rossi', 'mario@example.com', '3331234567', '1234', 'ADMIN');
+VALUES ('mario.rossi', 'mario@example.com', '3331234567', '$2a$10$geUpqolQlseNZngUZWW9ruKCBy1kYmuc8.FIMSxekYK4AzbuEeVqa', 'ADMIN');
 
 INSERT INTO utente (nome_utente, email, telefono, ruolo)
 VALUES ('anna.bianchi', 'anna@example.com', '3459876543', 'USER');
