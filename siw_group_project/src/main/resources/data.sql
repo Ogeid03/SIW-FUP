5b86ca84a021cff42f013e24262cdea9791567bc
Ogeid03 <45824052+Ogeid03@users.noreply.github.com>
2025-05-19 22:42:09 +0200

    confronto

--- siw_group_project/src/main/resources/data.sql
+++ siw_group_project/src/main/resources/data.sql
@@ -1,25 +1,10 @@
 INSERT INTO utente (nome_utente, email, telefono, password, ruolo)
 VALUES ('mario.rossi', 'mario@example.com', '3331234567', '$2a$10$nbt3iT4Zc2Sy5DQLmG7P2eeui2AsAilZgc9TrKk/vYCuMfexwBYza', 'ADMIN');
 
-INSERT INTO utente (nome_utente, email, telefono, ruolo)
-VALUES ('anna.bianchi', 'anna@example.com', '3459876543', 'USER');
+INSERT INTO utente (nome_utente, email, telefono, password, ruolo)
+VALUES ('anna.bianchi', 'anna@example.com', '3459876543', '$2a$10$nbt3iT4Zc2Sy5DQLmG7P2eeui2AsAilZgc9TrKk/vYCuMfexwBYza', 'USER');
 
-INSERT INTO avvistamento (
-    id,
-    data_ora,
-    specie,
-    razza,
-    descrizione_fisica,
-    foto,
-    informazioni_extra,
-    luogo,
-    latitudine,
-    longitudine,
-    cod_status,
-    cod_utente,
-    stato_salute,
-    azioni_intraprese
-) VALUES (
+INSERT INTO avvistamento (id, data_ora, specie, razza, descrizione_fisica, foto, informazioni_extra, luogo, latitudine, longitudine, cod_status, cod_utente, stato_salute, azioni_intraprese) VALUES (
     1001,
     '2025-05-19T08:45:00',
     'Cane',
@@ -32,6 +17,23 @@ INSERT INTO avvistamento (
     12.4800,
     'ATTIVO',
     1,
+    'Sembra in buona salute ma diffidente',
+    'Segnalato su app e lasciato un po’ di cibo'
+);
+
+INSERT INTO avvistamento (id, data_ora, specie, razza, descrizione_fisica, foto, informazioni_extra, luogo, latitudine, longitudine, cod_status, cod_utente, stato_salute, azioni_intraprese) VALUES (
+    1002,
+    '2025-05-19T08:45:00',
+    'Cane',
+    'Labrador',
+    'Labrador chocolate con occhi verdi',
+    'https://www.quattrozampeinfamiglia.it/wp-content/uploads/2024/10/cani-da-riporto.jpg',
+    'Senza collare, si aggirava nei pressi di un parcheggio',
+    'Via del Corso 123, Roma',
+    41.9000,
+    12.4800,
+    'ATTIVO',
+    2,
     'Sembra in buona salute ma diffidente',
     'Segnalato su app e lasciato un po’ di cibo'
 );
