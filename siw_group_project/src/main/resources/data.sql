648c0867cb26a7bf99b35e48637380fbd4d2187f
TeoCerre <matteo.cerretani@gmail.com>
2025-05-19 12:38:22 +0200

    datiPreinseriti

--- siw_group_project/src/main/resources/data.sql
+++ siw_group_project/src/main/resources/data.sql
@@ -4,3 +4,34 @@ VALUES ('mario.rossi', 'mario@example.com', '3331234567', '$2a$10$nbt3iT4Zc2Sy5D
 INSERT INTO utente (nome_utente, email, telefono, ruolo)
 VALUES ('anna.bianchi', 'anna@example.com', '3459876543', 'USER');
 
+INSERT INTO avvistamento (
+    id,
+    data_ora,
+    specie,
+    razza,
+    descrizione_fisica,
+    foto,
+    informazioni_extra,
+    luogo,
+    latitudine,
+    longitudine,
+    cod_status,
+    cod_utente,
+    stato_salute,
+    azioni_intraprese
+) VALUES (
+    1001,
+    '2025-05-19T08:45:00',
+    'Cane',
+    'Labrador',
+    'Labrador chocolate con occhi verdi',
+    'https://www.quattrozampeinfamiglia.it/wp-content/uploads/2024/10/cani-da-riporto.jpg',
+    'Senza collare, si aggirava nei pressi di un parcheggio',
+    'Via del Corso 123, Roma',
+    41.9000,
+    12.4800,
+    'ATTIVO',
+    1,
+    'Sembra in buona salute ma diffidente',
+    'Segnalato su app e lasciato un po’ di cibo'
+);
